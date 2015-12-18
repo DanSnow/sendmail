afa5387e8bbe75e5d157ff553da7b196a3a20240
DanSnow <dododavid006@gmail.com>
2015-12-18 15:58:07 +0800

    Add logger

---
+++ logger.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'logging'
+
+require_relative 'config'
+
+APP_LOGGER = 'sendmail'
+
+logger = Logging.logger[APP_LOGGER]
+logger.level = Config.log.to_sym
+
+logger.add_appenders Logging.appenders.stdout, Logging.appenders.file('mail.log')
+
+logger.info 'Start logger'
